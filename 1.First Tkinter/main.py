bb524c4a6bc3218468d865b5d5f4ced76446ee1d
KavyaJP <kavya31052005@gmail.com>
2025-02-23 17:25:46 +0530

    1st Tkinter Program

---
+++ 1.First Tkinter/main.py
@@ -0,0 +1,11 @@
+# Importing required modules
+from tkinter import *
+
+# Creating a window
+window = Tk()
+
+# Setting the title of the window
+window.title("First GUI Application")
+
+# Opening the Window
+window.mainloop()
